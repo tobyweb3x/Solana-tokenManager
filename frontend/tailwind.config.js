b1a20be2dfaaa96f68bf75975e69f90593a731c3
tobigiwa <giwaoluwatobi@gmail.com>
2024-10-09 15:30:56 +0100

    dashboard page include
    Signed-off-by: tobigiwa <giwaoluwatobi@gmail.com>

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/*.templ"],
+  theme: {
+    extend: {
+      spacing: {
+      },
+       fontFamily: {
+            },
+       colors: {
+      },
+      backgroundImage: {
+      },
+    },
+  },
+  plugins: [],
+}
+
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module token22Manager
 
 go 1.22.1
+
+require github.com/a-h/templ v0.2.778 // indirect
